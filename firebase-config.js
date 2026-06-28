2c247e75e2596b29334d0d80e2fca4b6d08fbf23
Artem <arty.codingart@gmail.com>
2026-06-28 14:34:32 +0500

    chore: configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 window.giftRegistryFirebase = {
   config: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: 'AIzaSyAlGhGZQ_k3ebbFFpRGLxMeGsaUWxzEpSQ',
+    authDomain: 'testnotification-dfd68.firebaseapp.com',
+    projectId: 'testnotification-dfd68',
+    storageBucket: 'testnotification-dfd68.firebasestorage.app',
+    messagingSenderId: '678105336022',
+    appId: '1:678105336022:web:869382971b601b3434a177',
+    measurementId: 'G-GLJBJT1639'
   }
 };
 
